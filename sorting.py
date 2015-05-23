a6d752554b5b6ee4653a13ef2e40299f36bd62c8
Lukas Rascius <lrascius93@gmail.com>
2015-05-23 14:17:55 -0400

    Adding selection sort

--- sorting.py
+++ sorting.py
@@ -11,7 +11,17 @@ def BubbleSort(array):
 				Swap(array, j, j+1)
 	return array
 
+def SelectionSort(array):
+	minimum = 0
+	for i in range(len(array)-1):
+		minimum = i
+		for j in range(i+1, len(array)):
+			if array[j] < array[minimum]:
+			 	minimum = j
+		if(minimum != i):
+			Swap(array, i, minimum)
 
+	return array
 
-print BubbleSort([4, 32, 3, 1])
-		
+print BubbleSort([4, 32, 4, 43, 3, 2, 3, 1])
+print SelectionSort([4, 32, 4, 43, 3, 2, 3, 1])
