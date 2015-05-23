c6e72ec143243306777865eadb4746e9cbbacec6
Lukas Rascius <lrascius93@gmail.com>
2015-05-23 15:23:50 -0400

    Adding merge sort

--- sorting.py
+++ sorting.py
@@ -1,3 +1,5 @@
+import math
+
 
 def Swap(array, i, j):
 	temp = array[i]
@@ -31,6 +33,43 @@ def InsertionSort(array):
 			j-=1
 	return array
 
+def Merge(left, right):
+	ret_array = []
+	left_index = 0
+	right_index = 0
+	while left_index != len(left) and right_index != len(right):
+		if left[left_index] <= right[right_index]:
+			ret_array.append(left[left_index])
+			left_index+=1
+		else:
+			ret_array.append(right[right_index])
+			right_index+=1
+	if(left_index == len(left)):
+		ret_array.extend(right[right_index:len(right)])
+	else:
+		ret_array.extend(left[left_index:len(left)])
+
+	return ret_array
+	
+
+def MergeSort(array):
+    if (len(array) == 1):
+        return array
+ 
+    left = MergeSort(array[0:len(array) / 2])
+    right = MergeSort(array[len(array) / 2: len(array)])
+    return Merge(left, right)
+
 print BubbleSort([4, 32, 4, 43, 3, 2, 3, 1])
 print SelectionSort([4, 32, 4, 43, 3, 2, 3, 1])
 print InsertionSort([4, 32, 4, 43, 3, 2, 3, 1])
+print MergeSort([4, 32, 4, 43, 3, 2, 3, 1])
+# array = [4, 32, 4, 43, 3, 2, 3, 1]
+# array.append(1)
+# print array
+
+# array1 = [1,2,3,4]
+# array2 = [3,4,5,6]
+# print Merge(array1, array2)
+# print math.floor(len([4, 32, 4, 43, 3, 2, 3, 1])/ 3)
+# print (len([1, 2, 3, 4]) // 2)
